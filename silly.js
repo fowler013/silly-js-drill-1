3ad5b4348b6bc2382e5c0dcd7779fc61609a5818
fowler013 <fowler0613@gmail.com>
2018-05-11 10:56:59 -0500

    initial commit

--- silly.js
+++ silly.js
@@ -6,9 +6,9 @@ function sing(){
     for( i = 0; i < friends.length; i++){
         for(j=99; j >0; j--){
             if(j>2){
-                console.log(j + " lines of code in the file," + j + "lines of code;" + friends[i] +" " + "strikes one out, clears it all out" + (j-1) +"line of code in the file");
+                console.log(j + " lines of code in the file," + j + "lines of code;" + friends[i] +" " + "strikes one out, clears it all out" + (j-1) + " " + "line of code in the file");
             }else if (j==2){
-                console.log(j + " lines of code in the file," + j + "lines  of code;"+ friends[i] + " " + "strikes one out, clears it all out" + (j-1) +"lines of code in the file");
+                console.log(j + " lines of code in the file," + j + "lines  of code;"+ friends[i] + " " + "strikes one out, clears it all out" + (j-1) + " " +"lines of code in the file");
             }else{
                 console.log( j + " lines of codes in the file," + j + "lines of code;" + friends[i] + " " + "strikes one out, clears it all out, no more lines of code in the file.");
             }
